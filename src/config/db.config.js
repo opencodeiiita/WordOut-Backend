54b3523b32e7f29c43fa93ccd47469ddb0a39f52
dark-dev2475 <paagalhaikya2475@gmail.com>
2024-12-16 01:06:55 +0530

    boilerplate for connection to mongodb

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,13 +2,22 @@ require('dotenv').config();  // Load environment variables
 
 const mongoose = require('mongoose');
 
-// Connect to MongoDB using the environment variable
-mongoose.connect(`${process.env.MONGO_URI}/ecommerce`)
+
+if (!process.env.MONGO_URI) {
+  console.error("MONGO_URI is not defined in environment variables");
+  process.exit(1);  
+}
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(function() {
     console.log("connected to mongo db");
   })
   .catch(function(err) {
-    console.log(err);
+    console.error("Error connecting to MongoDB:", err);
   });
 
 module.exports = mongoose.connection;
